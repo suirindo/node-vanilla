6bb65f9503578f752355ace07c29c04abf44b53f
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-16 02:33:48 +0900

    fina

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const http = require("http");
 
 http.createServer(function (req, res) {
-    if(req.url === "/") {
         res.setHeader("Content-Type", "text/plain;charset=utf-8");
+        res.setHeader("Content-Type", "text/html");
+        if(req.url === "/" && req.method === "GET") {
         res.write("こんにちは");
         res.end();
-    } else if(req.url === "/about") {
-        res.setHeader("COntent-Type", "text/plain;charset=utf-8");
+    } else if(req.url === "/about" && req.method === "GET" ) {
         res.write("ここはAboutページです。");
         res.end();
-    } else if(req.url === "/hobby") {
-        res.setHeader("Content-Type", "text/html");
+    } else if(req.url === "/hobby" && req.method === "GET" ) {
         res.write('<form action = "/outdoor" method = "POST"><input type = "text" name = "sports"> <button type = "submit">Submit</button></form> ');
         res.end();
-    } else if(req.url === "/outdoor") {
+    } else if(req.url === "/outdoor" && req.method === "POST" ) {
         console.log("reqの中身:", req);
     }
 }).listen(4000, function() {
